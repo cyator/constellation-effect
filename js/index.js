a7172cb4b2b37ec49d7c20030368fbe2f2b87def
c-yator <christian.yator@students.jkuat.ac.ke>
2021-10-19 15:05:16 +0300

    canstellation class

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
-import { handleParticles } from './handleParticles.js';
-import Particle from './particle.js';
+import CanstellationEffect from './Canstellation.js';
 
 let canvas;
 let ctx;
 let particleArray = [];
-let hue = 0;
-let interval = 1000 / 60;
-let timer = 0;
-let lastTime = 0;
-let canstellation;
+let canstellationAnimation;
+let canstellationEffect;
 
 const mouse = {
 	x: undefined,
@@ -20,21 +16,31 @@ window.onload = () => {
 	ctx = canvas.getContext('2d');
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	animate(0);
+	canstellationEffect = new CanstellationEffect(
+		ctx,
+		canvas.width,
+		canvas.height
+	);
+	canstellationAnimation = canstellationEffect.animate(0);
 };
 
 window.addEventListener('resize', () => {
-	cancelAnimationFrame(canstellation);
+	cancelAnimationFrame(canstellationAnimation);
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	animate(0);
+	canstellationEffect = new CanstellationEffect(
+		ctx,
+		canvas.width,
+		canvas.height
+	);
+	canstellationEffect.animate(0);
 });
 
 window.addEventListener('click', (e) => {
 	mouse.x = e.x;
 	mouse.y = e.y;
 	for (let i = 0; i < 2; i++) {
-		particleArray.push(new Particle(mouse, hue));
+		canstellationEffect.update(mouse);
 	}
 });
 
@@ -42,20 +48,6 @@ window.addEventListener('mousemove', (e) => {
 	mouse.x = e.x;
 	mouse.y = e.y;
 	for (let i = 0; i < 2; i++) {
-		particleArray.push(new Particle(mouse, hue));
+		canstellationEffect.update(mouse);
 	}
 });
-
-function animate(timeStamp) {
-	const deltaTime = timeStamp - lastTime;
-	lastTime = timeStamp;
-	if (timer > interval) {
-		ctx.fillStyle = 'rgba(0,0,0,0.02)';
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-		handleParticles(particleArray, ctx);
-		hue += 0.5;
-	} else {
-		timer += deltaTime;
-	}
-	canstellation = requestAnimationFrame(animate);
-}
